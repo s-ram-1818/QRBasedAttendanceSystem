f2a63d895580f29ce3271f26c95cdc90ea7a12c1
s-ram-1818 <ramsarwade1818@gmail.com>
2025-07-10 11:18:13 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://smart-attendance.onrender.com", credentials: true })
+);
 app.use(express.json());
 app.use(cookieParser());
 connectDB(); // Connect MongoDB
